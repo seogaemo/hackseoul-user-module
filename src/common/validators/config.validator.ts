db5492cce48a377d52add01e1b41ff2b1828dd03
suk-6 <me@suk.kr>
2024-08-25 00:20:32 +0900

    chore: Add required DATABASE_URL validation to config

--- src/common/validators/config.validator.ts
+++ src/common/validators/config.validator.ts
@@ -4,4 +4,6 @@ export const ConfigValidator = Joi.object({
   NODE_ENV: Joi.string()
     .valid("development", "production", "test")
     .default("production"),
+  // DB
+  DATABASE_URL: Joi.string().required(),
 });

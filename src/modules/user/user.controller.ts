f732e1d4553b92a3b5472ee88e85394d4737731a
suk-6 <me@suk.kr>
2024-08-24 23:10:43 +0900

    feat: Add user module, controller, and service for user management

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,43 @@
+import { status } from "@grpc/grpc-js";
+import {
+  CreateUser,
+  CreateUserResponse,
+  GetUserById,
+  UserResponse,
+  UserServiceController,
+  UserServiceControllerMethods,
+} from "@shared/generated/user.proto";
+import { Observable } from "rxjs";
+
+import { Controller } from "@nestjs/common";
+import { RpcException } from "@nestjs/microservices";
+
+import { UserService } from "./user.service";
+
+@Controller()
+@UserServiceControllerMethods()
+export class UserController implements UserServiceController {
+  constructor(private readonly userService: UserService) {}
+
+  getUserById(
+    request: GetUserById,
+  ): Promise<UserResponse> | Observable<UserResponse> | UserResponse {
+    return this.userService.getUserById(request.uid);
+  }
+
+  createUser(
+    request: CreateUser,
+  ):
+    | Promise<CreateUserResponse>
+    | Observable<CreateUserResponse>
+    | CreateUserResponse {
+    if (!request.user) {
+      throw new RpcException({
+        code: status.INVALID_ARGUMENT,
+        message: "User not provided",
+      });
+    }
+
+    return this.userService.createUser(request.user);
+  }
+}
---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,74 @@
+import { status } from "@grpc/grpc-js";
+import { Role as PrismaRole } from "@prisma/client";
+import {
+  CreateUserRequest,
+  CreateUserResponse,
+  Role as ProtoRole,
+  UserResponse,
+} from "@shared/generated/user.proto";
+import { hash } from "bcrypt";
+
+import { Injectable } from "@nestjs/common";
+import { RpcException } from "@nestjs/microservices";
+
+import { PrismaService } from "src/common/modules/prisma/prisma.service";
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  convertProtoRoleToPrismaRole(role: ProtoRole): PrismaRole {
+    switch (role) {
+      case ProtoRole.ADMIN:
+        return PrismaRole.ADMIN;
+      case ProtoRole.DISTRIBUTOR:
+        return PrismaRole.DISTRIBUTOR;
+      default:
+        throw new RpcException("Role not recognized");
+    }
+  }
+
+  convertPrismaRoleToProtoRole(role: PrismaRole): ProtoRole {
+    switch (role) {
+      case PrismaRole.ADMIN:
+        return ProtoRole.ADMIN;
+      case PrismaRole.DISTRIBUTOR:
+        return ProtoRole.DISTRIBUTOR;
+      default:
+        throw new RpcException("Role not recognized");
+    }
+  }
+
+  async getUserById(uid: string): Promise<UserResponse> {
+    const user = await this.prismaService.user.findUnique({
+      where: { uid },
+    });
+
+    if (!user) {
+      throw new RpcException({
+        code: status.NOT_FOUND,
+        message: "User not found",
+      });
+    }
+
+    return {
+      uid: user.uid,
+      name: user.name,
+      id: user.id,
+      role: this.convertPrismaRoleToProtoRole(user.role),
+    };
+  }
+
+  async createUser(user: CreateUserRequest): Promise<CreateUserResponse> {
+    const res = await this.prismaService.user.create({
+      data: {
+        id: user.id,
+        password: await hash(user.password, 10),
+        name: user.name,
+        role: this.convertProtoRoleToPrismaRole(user.role),
+      },
+    });
+
+    return { uid: res.uid };
+  }
+}

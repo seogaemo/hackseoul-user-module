dd24e05c54da7b30326a80aa1aa101f26351516c
suk-6 <me@suk.kr>
2024-08-24 20:40:41 +0900

    chore: Update grpc module to use user options

--- src/common/providers/grpc.module.ts
+++ src/common/providers/grpc.module.ts
@@ -1,4 +1,4 @@
-import { grpcClientOptions } from "@shared/options/hello.option";
+import { grpcClientOptions } from "@shared/options/user.option";
 import { GrpcReflectionModule } from "nestjs-grpc-reflection";
 
 import { Module } from "@nestjs/common";
